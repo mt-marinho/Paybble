8fe5b9e0a21811d58120e131342d5c5eb12e2740
mt-marinho <mt.mrnh.esp@gmail.com>
2026-03-17 22:15:04 -0400

    (feat): criado service registration da camada de application

---
+++ Backend/Paybble/Core/Paybbles.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Paybble.Application.Features.Expenses.Commands.CreateExpense;
+
+namespace Paybble.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateExpenseCommand).Assembly));
+
+            return services;
+        }
+    }
+}
