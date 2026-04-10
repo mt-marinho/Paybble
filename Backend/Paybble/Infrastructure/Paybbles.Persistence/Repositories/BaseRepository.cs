02b92ca814fb857ec38fb703d07ce0360a619010
mt-marinho <mt.mrnh.esp@gmail.com>
2026-04-09 22:48:56 -0400

    criado base repository

---
+++ Backend/Paybble/Infrastructure/Paybbles.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Paybble.Application.Contracts.Persistence;
+
+namespace Paybble.Persistence.Repositories
+{
+    public class BaseRepository<T>(PaybbleDbComtext dbcontext) : IAsyncRepository<T> where T : class
+    {
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbcontext.Set<T>().FindAsync(id);
+        }
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await dbcontext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await dbcontext.Set<T>().AddAsync(entity);
+            await dbcontext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            dbcontext.Set<T>().Update(entity);
+            await dbcontext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> DeleteAsync(T entity)
+        {
+            dbcontext.Set<T>().Remove(entity);
+            await dbcontext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
